fe17f54c6e81579a63db3288a826e9965e7c65b5
Matías Emanuel Sanhueza <msanhueza@unrn.edu.ar>
2019-11-11 08:45:11 -0300

    🎨 Modifica el tema rid-unrn-theme
    - Elimina la transición del navbar del rid-unrn-theme.js

--- dspace/modules/jspui/src/main/webapp/static/js/rid-unrn-theme.js
+++ dspace/modules/jspui/src/main/webapp/static/js/rid-unrn-theme.js
@@ -1,20 +1,6 @@
-/*
- * Método de transición del navbar
- */
 $.noConflict();
 (function ($) {
   $(document).ready(function() {
-    // Efecto de transición para el Navbar 
-    $(window).scroll(function() {
-      // checks if window is scrolled more than 50px, adds/removes solid class
-      if($(this).scrollTop() > 50) { 
-        $('.navbar').addClass('navbar-solid');
-        $('#logo').attr('src', '/jspui/image/header/header_logo_unrn_negro.svg');
-      } else {
-        $('.navbar').removeClass('navbar-solid');
-        $('#logo').attr('src','/jspui/image/header/header_logo_unrn_blanco.svg');
-      }
-    });
     // Preguntas frecuentes
     const url = document.location.toString();
     if ( url.match('#') ) {
